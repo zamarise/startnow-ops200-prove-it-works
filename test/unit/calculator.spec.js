2dd4d670a34dedabdd77995febf4c27a8af5069b
Zamarise Montoya <zamarise@gmail.com>
2018-05-22 20:45:42 -0700

    Update test functionality

--- test/unit/calculator.spec.js
+++ test/unit/calculator.spec.js
@@ -15,4 +15,16 @@ describe('Calculator', () => {
   it('should add 2 + 2 together correctly', () => {
     expect(calculator.add(2, 2)).to.equal(4);
   });
+
+  it('should subtract 2 - 2 correctly', () => {
+    expect(calculator.subtract).to.exist;
+  });
+
+  it('should multiply 2 + 2 together correctly', () => {
+    expect(calculator.multiply(2, 2)).to.equal(4);
+  });
+
+  it('should divide 2 by 2 correctly', () => {
+    expect(calculator.divide(2, 2)).to.equal(1);
+  });
 });
